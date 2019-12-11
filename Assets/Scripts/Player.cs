01b0485a6db0f33d6af63867e9054a9c0e3662de
antoine odi <antoine.kenneth.odi@gmail.com>
2019-12-11 23:23:26 +1030

    initial commit. Trying to get a feel of 2D game development

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    [SerializeField] private float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 moveDirection = new Vector2(0, 0);
+
+        moveDirection.Set(speed, speed);
+        rb.velocity = moveDirection * Time.fixedDeltaTime;
+    }
+}
