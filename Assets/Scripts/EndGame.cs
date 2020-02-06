7e4b402925226571e347cb3404ae9ea449bba71d
antoine odi <antoine.kenneth.odi@gmail.com>
2020-02-07 04:08:39 +1030

    leaderboard and scoring system in progress

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -7,33 +7,64 @@ using UnityEditor;
 public class EndGame : MonoBehaviour
 {
     [SerializeField] private Text gameOverText = null;
+    [SerializeField] private Text highscoreText = null;
     [SerializeField] private Text endMessageText = null;
     private string gameOverString = "GAME OVER";
     [SerializeField] private float duration = 1f;
     private float timer = 0;
     private int stringNumber = 0;
     private int m_coins = 0;
+    private int m_score = 0;
     [SerializeField] private LevelGenerator m_generator = null;
-    [SerializeField] private Player m_player = null;
+    //[SerializeField] private Player m_player = null;
+    private bool m_newHighscore = false;
 
     private void OnEnable()
     {
         // Clears text messages and reset string counter
         gameOverText.text = "";
         endMessageText.text = "";
+        highscoreText.text = "";
         stringNumber = 0;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = duration;
+        timer = 10;
         m_generator = FindObjectOfType<LevelGenerator>();
         NullChecker();
+        m_newHighscore = true;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (m_newHighscore)
+            HighscoreInput();
+        else
+            GameOverMessage();
+    }
+
+    private void HighscoreInput()
+    {
+        highscoreText.text = "NEW HIGHSCORE";
+        int distance = (int) m_generator.distance;
+        endMessageText.text = distance +"m x 1 = " + distance + "\n";
+        endMessageText.text += m_coins + "coins x 50 = " + m_coins * 50 + "\n";
+        m_score = distance + m_coins * 50;
+        endMessageText.text += "Total score: " + m_score;
+        if (timer > 0)
+            timer -= Time.unscaledDeltaTime;
+        else
+        {
+            m_newHighscore = false;
+            timer = duration;
+            highscoreText.text = "";
+        }
+    }
+
+    private void GameOverMessage()
     {
         if (timer > 0)
             timer -= Time.unscaledDeltaTime;
@@ -73,6 +104,11 @@ public class EndGame : MonoBehaviour
             if (gameOverText == null)
             {   
                 EditorUtility.DisplayDialog("Error", "Game Over Text has not been set on End Game", "Exit");
+                errorOccurred = true;
+            }
+            if (highscoreText == null)
+            {   
+                EditorUtility.DisplayDialog("Error", "Highscore Text has not been set on End Game", "Exit");
                 errorOccurred = true;
             }
             if (endMessageText == null)
---
+++ Assets/Scripts/ScoringSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+
+public class ScoringSystem : MonoBehaviour
+{
+    [Serializable]
+    public class ScoreData
+    {
+        public string name = "";
+        public int score = 0;
+    }
+
+    private Player m_player = null;
+    private LevelGenerator m_generator = null;
+
+    [SerializeField] private Text text = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_player = FindObjectOfType<Player>();
+        m_generator = FindObjectOfType<LevelGenerator>();
+        if (text)
+            text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
